7b810639552e5a3cb336379d9ce183eb825d8dd9
Andrea Francia <andrea@andreafrancia.it>
2015-02-24 23:24:21 +0100

    Ora riesce a fare senza problemi il parsing delle header (parsing basato sulla posizione dei due punti

--- read_itaca.m
+++ read_itaca.m
@@ -1,23 +1,33 @@
-function [values, headers] = read_itaca(filename)
-headers=[];
+function [values, info] = read_itaca(filename)
+info=[];
 f = fopen(filename);
 for i = 1:55
     line = fgetl(f);
-    r = textscan(line, '%s %s');
-    name = r{1}{1};
-    if name(length(name)) == ":"
-        name=name(1:length(name)-1);
+    [name, value] = split_header_line(line);
+    if strcmp('EVENT_NAME', name)
+        info = setfield(info, name, value);
+    elseif strcmp('EVENT_ID',name)
+        info = setfield(info, name, value);
+    elseif strcmp('EVENT_DATE_YYYYMMDD',name)
+        info = setfield(info, name, value);
+    elseif strcmp('EVENT_TIME_HHMMSS',name)
+        info = setfield(info, name, value);
+    elseif strcmp('MAGNITUDE_L',name)
+        value = sscanf(value, '%f');
+        info = setfield(info, name, value);
+    elseif strcmp('STATION_NAME',name)
+        info = setfield(info, name, value);
     end
-    if length(r{2})
-        value = r{2}{1};
-        if sscanf(value, '%f')
-            value = sscanf(value, '%f');
-        end
-        headers = setfield(headers, name, value);
-    end
-    
 end
 
 values = textscan(f, '%f');
 values = values{1};
+end
+
+function [name, value] = split_header_line(line)
+
+colon_position = strfind(line, ':');
+name=line(1:colon_position-1);
+value=strtrim(line(colon_position+1:length(line)));
+
 end
